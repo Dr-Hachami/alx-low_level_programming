723bcd5756e4e1a4f3a3502dd9cb625e31400e61
Yassine <evilnotop18@gmail.com>
2023-06-26 11:31:48 +0100

    new working file

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 9
+
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i, sum;
+
+    srand(time(NULL));
+
+    for (i = 0, sum = 0; sum <= 2772 - 122; i++)
+    {
+        password[i] = rand() % 94 + 33;
+        sum += password[i];
+    }
+
+    password[i] = 2772 - sum;
+
+    printf("%s\n", password);
+
+    return 0;
+}
+
